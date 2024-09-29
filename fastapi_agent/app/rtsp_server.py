147c4a02428cd00c2969eac7cc0cdd5767d2e88f
SongInYong <30370933+songinyong@users.noreply.github.com>
2024-09-30 07:45:20 +0900

    유저 파이프라인 조작 제어 코드 부분 수정

--- fastapi_agent/app/rtsp_server.py
+++ fastapi_agent/app/rtsp_server.py
@@ -14,53 +14,59 @@ gi.require_version('Gst', '1.0')
 gi.require_version('GstRtspServer', '1.0')
 from gi.repository import Gst, GstRtspServer, GObject
 
-class RTSPServer(threading.Thread):
-    def __init__(self, port=8554, mount_point="/test"):
-        super().__init__()
-        Gst.init(None)
-        self.loop = GObject.MainLoop()
-        self.server = GstRtspServer.RTSPServer()
-        self.server.props.service = str(port)
-        self.is_streaming = False
 
-        self.server.props.address = '0.0.0.0' # 접근 허용 IP
-        self.mount_point = mount_point 
+# Gstreamer 의 CustomRTSPMediaFactory 상속 받아서 유저가 동적으로 API 호출하여 파이프라인 제어
+class CustomRTSPMediaFactory(GstRtspServer.RTSPMediaFactory):
+    def __init__(self, device, **properties):
+        super(CustomRTSPMediaFactory, self).__init__(**properties)
+        self.device = device
+        self.pipeline = None
 
-        self.factory = GstRtspServer.RTSPMediaFactory()
-        self.factory.set_shared(True)
+    def do_create_element(self, url):
+        pipeline_str = f"( v4l2src device={self.device} ! videoconvert ! x264enc ! rtph264pay name=pay0 pt=96 )"
+        self.pipeline = Gst.parse_launch(pipeline_str)
+        return self.pipeline
 
+    def stop(self):
         # 초기화 시점에 빈 영상을 송출하도록 launch 라인 설정
         #pipeline_str = "( videotestsrc pattern=black ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay name=pay0 pt=96 )"
-        self.factory.set_launch(None)
+        if self.pipeline:
+            self.pipeline.set_state(Gst.State.NULL)
+            self.pipeline = None
 
-        self.mount_points = self.server.get_mount_points()
-        self.mount_points.add_factory(mount_point, self.factory)
+class RTSPServer:
+    def __init__(self, port=8554, mount_point="/test"):
+        Gst.init(None)
+        self.server = GstRtspServer.RTSPServer()
+        self.server.props.service = str(port)
         self.mount_point = mount_point
+        self.factory = None
+        self.is_streaming = False
 
-    def run(self):
+    def start(self):
         self.server.attach(None)
-        self.loop.run()
 
     def start_stream(self, device='/dev/video0'):
         if self.is_streaming:
             print("Streaming is already in progress.")
             return
-        # 실제 카메라 데이터 송출
-        pipeline_str = f"( v4l2src device={device} ! videoconvert ! queue ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay name=pay0 pt=96 )"
-        self.factory.set_launch(pipeline_str)
+
+        self.factory = CustomRTSPMediaFactory(device)
+        self.factory.set_shared(True)
+        mounts = self.server.get_mount_points()
+        mounts.add_factory(self.mount_point, self.factory)
         self.is_streaming = True
-        print(f"Streaming has started on device {device}.")
 
     def stop_stream(self):
         if not self.is_streaming:
             print("Streaming is not in progress.")
             return
-        # 빈 영상을 송출하는 파이프라인으로 전환
-        #pipeline_str = "( videotestsrc pattern=black ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay name=pay0 pt=96 )"
-        self.factory.set_launch(None)
+
+        if self.factory:
+            self.factory.stop()
+            # Mount point에서 팩토리 제거
+            mounts = self.server.get_mount_points()
+            mounts.remove_factory(self.mount_point)
+            self.factory = None
+
         self.is_streaming = False
-        print("Streaming has stopped. A blank video is being transmitted.")
-
-    def get_stream_uri(self):
-        return f"rtsp://{self.server.props.address}:{self.server.props.service}{self.mount_point}"
-
